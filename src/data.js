d54dbb2230aa01d6fe54a39ee419c1b8864de1c7
dan2811 <djordandrums@gmail.com>
2022-01-05 15:43:00 +0000

    Add slider and categories

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://1f0f7g2kx42449whb51s543o-wpengine.netdna-ssl.com/wp-content/uploads/2018/12/big-sale2.jpg",
+        title: "SALE NOW ON",
+        desc: "ALL THE LATEST TRENDS AT BARGAIN PRICES!",
+        bg: "f7f6f4",
+    },
+    {
+        id: 2,
+        img: "https://p0.pxfuel.com/preview/1006/203/797/the-man-in-the-suit-costume-man-guy.jpg",
+        title: "SUITS",
+        desc: "THE VERY FINEST IN MODERN MEN'S SUITS.",
+        bg: "F55555",
+    },
+    {
+        id: 3,
+        img: "https://i.etsystatic.com/8957419/r/il/48d852/3264378987/il_794xN.3264378987_rjxi.jpg",
+        title: "TROUSERS",
+        desc: "COMFORT. MOBILITY. STYLE.",
+        bg: "f44498",
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://cdn.shopify.com/s/files/1/0258/6024/3538/collections/Mens_Clothes_Sale_1200x1200.jpg?v=1589623819",
+        title: "SALE",
+    },
+    {
+        id: 2,
+        img: "https://p0.pxfuel.com/preview/1006/203/797/the-man-in-the-suit-costume-man-guy.jpg",
+        title: "SUITS",
+    },
+    {
+        id: 3,
+        img: "https://i.etsystatic.com/8957419/r/il/48d852/3264378987/il_794xN.3264378987_rjxi.jpg",
+        title: "TROUSERS",
+    }
+
+];
