1c8da9649f36fbd3c718270715faadd9c98f4bcf
dan2811 <djordandrums@gmail.com>
2022-01-11 12:17:37 +0000

    connect front end to api to retrieve products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
+import axios from "axios";
 
 const Container = styled.div`
 padding: 20px;
@@ -10,9 +12,37 @@ justify-content: space-between;
 `
 
 const Products = ({cat, filters, sort}) => {
+
+    const [products, setProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try{
+                const res = await axios.get( cat ? `http://localhost:5000/api/products?category=${cat}`
+                                                    : "http://localhost:5000/api/products"
+                                                    );
+                setProducts(res.data);
+            } catch (err) {}
+        };
+        getProducts();
+     }, [cat]);
+
+     useEffect(() => {
+         cat && setFilteredProducts(
+             products.filter((item) => 
+             Object.entries(filters).every(([key, value])=>
+             item[key].includes(value)
+             )
+             )
+         );
+     }, [products, cat, filters]);
+
+
+
     return (
         <Container>
-            {popularProducts.map(item=>(
+            {filteredProducts.map(item=>(
                 <Product item={item} key={item.id}/>
             ))}
         </Container>
